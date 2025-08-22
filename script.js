da34a9a137ff26b3784c8e7a6d1eaa4a7c29b12f
Amina <107080579+Aminalo@users.noreply.github.com>
2025-08-22 10:27:27 -0400

    add starter scaffolding

---
+++ script.js
@@ -0,0 +1,7 @@
+// Importing database functions. 
+import { central, db1, db2, db3, vault } from "./databases";
+
+// Part 1 scaffold: map db keys and export a function name only
+export async function getUserData(id) {
+
+}
